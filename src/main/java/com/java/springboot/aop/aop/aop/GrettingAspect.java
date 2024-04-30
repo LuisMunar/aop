afd9ccfe5ce09b30be6798adcb1218d39f8f0dc0
LuisMunar <luis.emunarr@gmail.com>
2024-04-30 07:06:44 -0500

    Video 107 ready

--- src/main/java/com/java/springboot/aop/aop/aop/GrettingAspect.java
+++ src/main/java/com/java/springboot/aop/aop/aop/GrettingAspect.java
@@ -3,6 +3,7 @@ package com.java.springboot.aop.aop.aop;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.After;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -17,5 +18,12 @@ public class GrettingAspect {
     String methodName = joinPoint.getSignature().getName();
     String args = joinPoint.getArgs().toString();
     logger.info("Before => " + methodName + " => " + args);
+  }
+
+  @After("execution(* com.java.springboot.aop.aop.services.GrettingService.greet(..))")
+  public void afterGreeting(JoinPoint joinPoint) {
+    String methodName = joinPoint.getSignature().getName();
+    String args = joinPoint.getArgs().toString();
+    logger.info("After => " + methodName + " => " + args);
   }
 }
